d2296df578dca1e7a05662366b8a7f6513885e17
MALSHANI NANAYAKKARA <nanayakkara.nbtmn@MOF.local>
2023-08-15 16:41:32 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+
+
+<template>
+  <div class="flexbox-container flex">
+    <div class="flexbox-item flexbox-item-1 w-1/2 h-screen bg-[#2B6CB0]">
+        <div class="flexbox-item flexbox-item-1 h-64">
+           <div class="h1 font-sans text-white text-xl font-bold">Chakra Pro</div>
+        </div>
+        <div class="flexbox-item flexbox-item-2 w-full h-96">
+            <div class="h5 font-sans text-white text-6xl font-bold">Start making your dreams come true</div>
+            <div class="p font-sans text-white text-xl">Create an account and discover the worlds' best UI component framework.</div>
+        </div>
+        <div class="flexbox-item flexbox-item-3 w-full">
+            <div class="p font-sans text-white text-sm">© 2022 Chakra UI. All rights reserved.</div>
+        </div>
+    </div>
+
+    <div class="flexbox-item flexbox-item-2 w-1/2 h-screen">
+        ff
+    </div>
+
+</div>
+</template>
+
+
+

29022dfa5cdca5ed56cfcacc4ef6744eeefc4a76
MALSHANI NANAYAKKARA <nanayakkara.nbtmn@MOF.local>
2023-08-22 16:12:58 +0530

    initialize

--- src/App.vue
+++ src/App.vue
@@ -1,65 +1,81 @@
 
 
 <template>
-  <div class="flexbox-container flex">
-    <div class="flexbox-item flexbox-item-1 w-1/2 h-screen bg-[#2B6CB0]">
-        <div class="flexbox-item flexbox-item-1 h-64">
-           <div class="h1 font-sans text-white text-2xl font-bold pl-56 pt-4">Chakra PRO</div>
-        </div>
-        <div class="flexbox-item flexbox-item-2 w-full h-96">
-            <div class="h5 font-sans text-white text-6xl font-bold pl-56 pt-8 pr-48">Start making your dreams come true</div>
-            <div class="p font-sans text-white text-xl pl-56 pt-4 pr-48 ">Create an account and discover the worlds' best UI component framework.</div>
-            <div class="p font-sans text-white text-xl pl-56 pt-4 pr-48 ">Join 10.000+ users</div>
-        </div>
-        <div class="flexbox-item flexbox-item-3 w-full">
-            <div class="p font-sans text-white text-sm absolute bottom-[20px] pl-56 pt-4 pb-4">© 2022 Chakra UI. All rights reserved.</div>
-        </div>
-    </div>
-
-    <div class="flexbox-item flexbox-item-2 w-1/2 h-screen">
-        <div class="flexbox-item flexbox-item-1 h-64">&nbsp;</div>
-        
-        <div class="flexbox-item flexbox-item-2 h-96">
-            <div class="3/4 h-9 pl-48 pr-96 pt-2">
-                <div class="h5 font-sans text-black text-3xl font-bold text-center">Log in to your account</div>
-                <div class="p font-sans text-black text-lg pt-2 text-center">Don't have an account? <a href="#!" color="text-sky-700">Sign up</a></div>
-                <div class="">
-                <form>
-                    <div>
-                        <label class="block text-gray-700" for="email">Email</label>
-                        <input type="text" class="border-2 block w-full bg-transparent rounded-lg h-10" id="username" placeholder="  Enter your email">
-                    </div>
-                    <div class="pt-2">
-                        <label class="block text-gray-700" for="email">Password</label>
-                        <input type="password" class="border-2 block w-full bg-transparent rounded-lg h-10" id="password" placeholder="*********">
-                    </div>
-                    <div class="mb-6 flex items-center justify-between pt-3">
-                        <div>
-                            <input class="" type="checkbox" value="" id="checkbox">
-                            <label class="" >Remember Me</label>
-                        </div>
-                        <div class="text-sky-700">
-                            <a href="#!">Forgot password?</a>  
-                        </div>
-                    </div>
-                    <div>
-                        <button type="button" class="rounded-md block bg-[#2B6CB0] w-full h-10 text-white font-normal">Sign In</button>
-                    </div>
-                    <div class="pt-2">
-                        <button type="button" class="rounded-md block bg-white w-full h-10 font-normal border-2 ">Sign In with Google</button>
-                    </div>
-                </form>
-
-                </div>
+    <div class="flex flex-container w-screen h-screen space-x-2">
+        <div class="flex flex-col w-1/2 space-y-2 bg-[rgb(43,108,176)]">
+            <div class="flex flex-row h-1/3">
+                <div class="w-1/3"></div>
+                <div class="w-1/3 h1 font-sans text-white text-2xl font-bold pt-4">Chakra PRO</div>
+                <div class="w-1/3"></div>
             </div>
-            
+            <div class="flex flex-row h-1/3">
+                <div class="w-1/3"></div>
+                <div class="flex-row w-2/3">
+                    <div class="font-sans text-white text-6xl font-bold text-left">Start making your dreams come true</div>
+                    <br>
+                    <div class="h-1/3 p font-sans text-white text-xl">Create an account and discover the worlds' best UI
+                        <br> component framework.</div>
+                    <div class="h-1/3 font-sans text-white text-xl">Join 10.000+ users</div>
+                </div>
+                <div class=""></div>
+            </div>
+            <div class="h-1/3 flex flex-row items-end">
+                <div class="w-1/3"></div>
+                <div class="w-1/3 p font-sans text-white text-sm pb-4">© 2022 Chakra UI. All rights reserved.</div>
+                <div class="w-1/3"></div>
+            </div>
         </div>
-        
-        
+
+        <div class="flex flex-row w-1/2 space-x-2">
+            <div class="w-1/4"> </div>
+            <div class="flex-row w-2/4">
+                <div class="h-1/4"></div>
+                <div class="h-2/4">
+                    <div class="h5 font-sans text-black text-3xl font-bold text-center">Log in to your account</div>
+                    <div class="p font-sans text-black text-lg pt-2 text-center">Don't have an account? <a href="#!"
+                            color="text-sky-700">Sign up</a></div>
+                    <div class="">
+                        <form>
+                            <div>
+                                <label class="block text-gray-700" for="email">Email</label>
+                                <input type="text" class="border-2 block w-full bg-transparent rounded-lg h-10"
+                                    id="username" placeholder="  Enter your email">
+                            </div>
+                            <div class="pt-2">
+                                <label class="block text-gray-700" for="email">Password</label>
+                                <input type="password" class="border-2 block w-full bg-transparent rounded-lg h-10"
+                                    id="password" placeholder="*********">
+                            </div>
+                            <div class="mb-6 flex items-center justify-between pt-3">
+                                <div>
+                                    <input class="" type="checkbox" value="" id="checkbox">
+                                    <label class="">Remember Me</label>
+                                </div>
+                                <div class="text-sky-700">
+                                    <a href="#!">Forgot password?</a>
+                                </div>
+                            </div>
+                            <div>
+                                <button type="button"
+                                    class="rounded-md block bg-[#2B6CB0] w-full h-10 text-white font-normal">Sign
+                                    In</button>
+                            </div>
+                            <div class="pt-2">
+                                <button type="button"
+                                    class="rounded-md block bg-white w-full h-10 font-normal border-2 ">Sign In with
+                                    Google</button>
+                            </div>
+                        </form>
+
+                    </div>
+                </div>
+                <div class="h-1/4"></div>
+            </div>
+            <div class=" w-1/4"></div>
+
     </div>
 
-</div>
-</template>
+</div></template>
 
 
 
